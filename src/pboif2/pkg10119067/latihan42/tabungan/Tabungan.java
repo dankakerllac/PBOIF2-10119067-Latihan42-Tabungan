ee71d484dd703d187212f1399eb4dc85999521cb
User <User@DESKTOP-6N6EHH6>
2020-11-14 21:22:10 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan42.tabungan;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Tabungan
+ */
+public class Tabungan {
+    private final int saldo;
+
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return saldo - jumlah;
+    }
+}
